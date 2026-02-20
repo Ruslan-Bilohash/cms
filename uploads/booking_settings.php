8483e27dfe173635af54124393f99caa2bf45d43
Ruslan Bilohash <138953118+Ruslan-Bilohash@users.noreply.github.com>
2026-02-20 23:11:31 +0100

    Create booking_settings.php

---
+++ uploads/booking_settings.php
@@ -0,0 +1,38 @@
+<?php return array (
+  'currency' => 'UAH',
+  'min_price' => 50,
+  'max_price' => 5000,
+  'items_per_page' => 5,
+  'robots' => 'index, follow',
+  'description' => '',
+  'keywords' => '',
+  'footer_phone' => '46255885',
+  'footer_email' => 'rbilohash@gmail.com',
+  'footer_address' => 'GOTSKALK JONSONS VEI 1',
+  'footer_facebook' => 'https://facebook.com/',
+  'footer_instagram' => 'https://instagram.com',
+  'footer_twitter' => 'https://twitter.com',
+  'footer_telegram' => 'https://t.me/+37062222411',
+  'footer_site_name' => 'Готовый сайт + скрипт бронирования номеров онлайн — ваш отель в интернете за 1 день!',
+  'footer_navigation' => 
+  array (
+    0 => 
+    array (
+      'url' => '/',
+      'text' => 'Главная',
+      'icon' => 'fas fa-home',
+    ),
+    1 => 
+    array (
+      'url' => '/templates/default/booking.php',
+      'text' => 'Бронирование',
+      'icon' => 'fas fa-hotel',
+    ),
+    2 => 
+    array (
+      'url' => '/shop',
+      'text' => 'Pro Shop Скрипт',
+      'icon' => 'fas fa-shop',
+    ),
+  ),
+);
