173719873ffb8c5b8d7ee6a419c2c0fc202eef17
Ruslan Bilohash <138953118+Ruslan-Bilohash@users.noreply.github.com>
2026-02-20 23:10:31 +0100

    Create shop_seo_settings.php

---
+++ uploads/shop_seo_settings.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'shop_meta_title' => 'Pro Shop Management скрипт интернет-магазина ',
+  'shop_meta_desc' => 'Это мощное и эффективное решение для SEO-оптимизации, которое помогает вам не только привлекать больше посетителей.',
+  'shop_keywords' => 'Pro Shop, Management Engine, Наш скрипт для интернет-магазина, Скрипт интернет магазина SEO, Shop Engine Script',
+  'shop_og_title' => 'Pro Shop Management Engine скрипт интернет-магазина ',
+  'shop_og_desc' => 'Это мощное и эффективное решение для SEO-оптимизации, которое помогает вам не только привлекать больше посетителей.',
+  'category_title_template' => '{category_name} - Pro Shop Management Engine',
+  'product_title_template' => '{product_name} -  Наш скрипт для интернет-магазина ',
+);
