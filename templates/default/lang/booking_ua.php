02ac43b48197228dd542f641b2bbd548cce26303
Ruslan Bilohash <138953118+Ruslan-Bilohash@users.noreply.github.com>
2026-02-20 22:56:59 +0100

    Create booking_ua.php

---
+++ templates/default/lang/booking_ua.php
@@ -0,0 +1,59 @@
+<?php
+// templates/default/lang/booking_ua.php
+return [
+    'footer_contacts' => 'Контакти',
+    'footer_social' => 'Соціальні мережі',
+    'footer_navigation' => 'Навігація',
+    'footer_rights' => 'Усі права захищено.',
+    'footer_developed' => 'Розроблено',
+
+    'footer_nav_glavnaya' => 'Головна',
+    'footer_nav_bronirovanie' => 'Бронювання',
+
+    'meta_description' => 'Онлайн бронювання номерів - знайдіть ідеальне місце для відпочинку.',
+    'meta_keywords' => 'бронювання, номери, готель, відпочинок, резервування',
+
+    'title_booking' => 'Онлайн бронювання - Tender CMS',
+    'header_title' => 'Система онлайн бронювання',
+    'header_subtitle' => 'Знайдіть ідеальне місце для проживання',
+
+    'label_check_in' => 'Заїзд',
+    'label_check_out' => 'Виїзд',
+    'guests_suffix' => [1 => 'гість', 2 => 'гості', 3 => 'гостей'],
+    'btn_search' => 'Пошук',
+
+    'error_invalid_dates' => 'Невірні дати заїзду або виїзду.',
+    'error_no_rooms' => 'Немає доступних номерів за обраними параметрами.',
+    'error_no_rooms_dates' => 'Немає вільних номерів на обрані дати.',
+    'error_query_prepare' => 'Помилка підготовки запиту',
+    'error_query_execute' => 'Помилка виконання запиту',
+
+    'label_category' => 'Категорія',
+    'label_capacity' => 'Місткість',
+    'guests' => 'гостей',
+    'per_night' => 'ніч',
+    'btn_book' => 'Забронювати',
+
+    // booking_order.php
+    'title_order' => 'Оформлення бронювання',
+    'header_order_title' => 'Оформлення бронювання',
+    'header_order_subtitle' => 'Забронюйте своє проживання',
+
+    'error_room_not_found' => 'Номер не знайдено.',
+    'error_guests_exceed' => 'Кількість гостей перевищує місткість номера.',
+    'error_name_required' => 'Будь ласка, вкажіть ваше ім’я.',
+    'error_phone_invalid' => 'Будь ласка, вкажіть коректний номер телефону.',
+    'success_booking' => 'Бронювання успішно відправлено! Очікуйте підтвердження.',
+    'error_booking' => 'Помилка під час бронювання',
+    'error_prefix' => 'Помилка',
+
+    'label_price' => 'Ціна',
+    'section_your_details' => 'Ваші дані',
+    'label_name' => 'Ім’я',
+    'placeholder_name' => 'Введіть ваше ім’я',
+    'label_phone' => 'Телефон',
+    'placeholder_phone' => '+380123456789',
+    'label_guests' => 'Кількість гостей',
+    'section_other_rooms' => 'Інші доступні номери',
+    'up_to' => 'До',
+];
