74d787bc0399eed2d4b2a8b104ae1c95938126ae
Ruslan Bilohash <138953118+Ruslan-Bilohash@users.noreply.github.com>
2026-02-20 23:09:21 +0100

    Create shop_settings.php

---
+++ uploads/shop_settings.php
@@ -0,0 +1,184 @@
+<?php return array (
+  'shop_enabled' => 1,
+  'shop_dashboard_limit' => 10,
+  'shop_currency' => 'ГРН',
+  'footer' => 
+  array (
+    'logo_url' => '/uploads/shop/67fd905921a55-67f804ca7d328-mysql..webp',
+    'show_logo' => 1,
+    'logo_width' => 75,
+    'logo_height' => 75,
+    'text_position' => 'below',
+    'text_1' => 'Pro Site Management Engine!',
+    'show_text_1' => 0,
+    'text_1_icon' => 'fa-shop',
+    'text_1_icon_size' => 20,
+    'text_1_icon_color' => '#f906c4',
+    'phone' => '+4746255885',
+    'show_phone' => 0,
+    'phone_icon' => 'fa-phone',
+    'phone_icon_size' => 20,
+    'phone_icon_color' => '#f9db15',
+    'address' => 'Литва, Вильнюс',
+    'show_address' => 0,
+    'address_icon' => 'fa-map-marker-alt',
+    'address_icon_size' => 20,
+    'address_icon_color' => '#00fffb',
+    'email' => 'rbilohash@gmail.com',
+    'show_email' => 1,
+    'email_icon' => 'fa-envelope',
+    'email_icon_size' => 20,
+    'email_icon_color' => '#00ff62',
+    'working_hours' => 'Часы работы: Пн-Вс, 8:00-21:00',
+    'show_working_hours' => 0,
+    'working_hours_icon' => 'fa-clock',
+    'working_hours_icon_size' => 20,
+    'working_hours_icon_color' => '#ffffff',
+    'link_color' => '#fff700',
+    'bg_color_1' => '#2c3e50',
+    'bg_color_2' => '#1abc9c',
+    'text_color' => '#fafafa',
+    'font_size' => 18,
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'title' => 'Навигация',
+        'show_title' => 1,
+        'title_color' => '#ffffff',
+        'title_font_size' => 20,
+        'title_icon' => 'fa-list',
+        'title_icon_size' => 24,
+        'title_icon_color' => '#ffffff',
+        'items' => 
+        array (
+          0 => 
+          array (
+            'text' => 'Главная',
+            'link' => '/',
+            'icon' => 'fa-home',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          1 => 
+          array (
+            'text' => 'Интернет магазин',
+            'link' => '/shop',
+            'icon' => 'fa-book',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          2 => 
+          array (
+            'text' => 'Новости',
+            'link' => '/news',
+            'icon' => 'fa-video',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          3 => 
+          array (
+            'text' => 'Сервисные страницы',
+            'link' => '/all_pages',
+            'icon' => 'fa-users',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'title' => 'Информация',
+        'show_title' => 1,
+        'title_color' => '#ffffff',
+        'title_font_size' => 20,
+        'title_icon' => 'fa-info-circle',
+        'title_icon_size' => 24,
+        'title_icon_color' => '#ffffff',
+        'items' => 
+        array (
+          0 => 
+          array (
+            'text' => 'Модули оплаты',
+            'link' => '/pay',
+            'icon' => 'fa-dollar-sign',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          1 => 
+          array (
+            'text' => 'Шаблоны',
+            'link' => '/templates',
+            'icon' => 'fa-comment',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          2 => 
+          array (
+            'text' => 'Мультиязычность',
+            'link' => '/multilang',
+            'icon' => 'fa-blog',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          3 => 
+          array (
+            'text' => 'Справка',
+            'link' => '/',
+            'icon' => 'fa-question',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+        ),
+      ),
+      2 => 
+      array (
+        'title' => 'О нас',
+        'show_title' => 1,
+        'title_color' => '#ffffff',
+        'title_font_size' => 20,
+        'title_icon' => 'fa-users',
+        'title_icon_size' => 24,
+        'title_icon_color' => '#ffffff',
+        'items' => 
+        array (
+          0 => 
+          array (
+            'text' => 'О нас',
+            'link' => '/about',
+            'icon' => 'fa-info',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          1 => 
+          array (
+            'text' => 'Контакты',
+            'link' => '/contacts',
+            'icon' => 'fa-address-book',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          2 => 
+          array (
+            'text' => 'Поддержка',
+            'link' => '/support',
+            'icon' => 'fa-life-ring',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+          3 => 
+          array (
+            'text' => 'Карьера',
+            'link' => '/career',
+            'icon' => 'fa-briefcase',
+            'icon_size' => 18,
+            'icon_color' => '#ffffff',
+          ),
+        ),
+      ),
+    ),
+  ),
+  'show_products_on_index' => 0,
+  'index_products_limit' => 6,
+  'nova_poshta_enabled' => true,
+);
